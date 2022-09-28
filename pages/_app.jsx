58d14e4018fb4f2f91c7e172a462a56427e84c92
yao <yaozhou.wuhu@gmail.com>
2022-09-28 10:22:39 +0800

    first commit

---
+++ pages/_app.jsx
@@ -0,0 +1,9 @@
+// All rights reservered by sharonrherrin7@gmail.com
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
